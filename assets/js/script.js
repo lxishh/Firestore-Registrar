cf90b9d35a48241ece1f40d0d82c0a3115776a51
lxishh <henrix913@gmail.com>
2024-05-17 10:20:22 -0400

    FORMULARIO LISTO
    importante en el form que el type sea button o si no, no se envian los datos a la bd.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,18 @@ window.addEventListener("load",()=>{
 const registrar = ()=>{
     // Recuperar elemento
     let eNombre = document.getElementById("nombre");
+    let eApellido = document.getElementById("apellido");
+    let eNacimiento = document.getElementById("fnacimiento");
+    let eRut = document.getElementById("rut");
+    let eCorreo = document.getElementById("correo");
     // Recuperar el valor
     let vNombre = eNombre.value;
+    let vApellido = eApellido.value;
+    let vNacimiento = eNacimiento.value;
+    let vRut = eRut.value;
+    let vCorreo = eCorreo.value;
     // Crear un objeto
-    let objeto = {nombre:vNombre};
+    let objeto = {nombre:vNombre, apellido:vApellido, fnacimiento:vNacimiento, rut:vRut, correo:vCorreo};
     console.log(objeto)
     registrarPersona(objeto).then(()=>{
         alert("Registrado con éxito")
