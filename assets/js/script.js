9a1ff594ece5dc625da341ce9e4d1a38fd236cac
lxishh <henrix913@gmail.com>
2024-05-17 09:05:03 -0400

    Pre-promesas
    firebase

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load",()=>{
+    document.getElementById("btnRegistrar").addEventListener("click",registrar);
+});
+
+const registrar = ()=>{
+    // Recuperar elemento
+    let eNombre = document.getElementById("nombre");
+    // Recuperar el valor
+    let vNombre = eNombre.value;
+    // Crear un objeto
+    let objeto = {nombre:vNombre};
+    console.log(objeto)
+}
