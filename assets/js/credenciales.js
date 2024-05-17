e68be4d7a698bba892cc9715294da0b9dfbb617c
lxishh <henrix913@gmail.com>
2024-05-17 08:34:46 -0400

    Initial commit

---
+++ assets/js/credenciales.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDWm167zj-jAexzdg2mMhwvaasnw2-yZdo",
+    authDomain: "viernes-17-de-mayo.firebaseapp.com",
+    projectId: "viernes-17-de-mayo",
+    storageBucket: "viernes-17-de-mayo.appspot.com",
+    messagingSenderId: "513561739335",
+    appId: "1:513561739335:web:4b3d0406826d940e8a7162"
+  };
---
+++ assets/js/firebase.js
@@ -0,0 +1,10 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+import { firebaseConfig } from "./credenciales.js";
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
